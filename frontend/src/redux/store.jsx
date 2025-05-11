75e3d5720e029afb434231422d962c1a3a4a5128
Soumitra Dey <107596444+soumitra-dey@users.noreply.github.com>
2025-05-11 20:35:23 +0530

    Add placeholder reducer to Redux store

--- frontend/src/redux/store.jsx
+++ frontend/src/redux/store.jsx
@@ -1,10 +1,13 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import thunk from "redux-thunk";
 
-
+// Placeholder reducer until actual reducers are implemented
+const placeholderReducer = (state = null, action) => {
+    return state;
+};
 
 const rootReducer = combineReducers({
-    
-})
+    placeholder: placeholderReducer
+});
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
